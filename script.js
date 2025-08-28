baf238687b4a0b63bbbc7d30111c04b3cb24559c
PhilipN55 <228013432+PhilipN55@users.noreply.github.com>
2025-08-28 22:51:40 +0200

    html 0.1.1

---
+++ script.js
@@ -0,0 +1,20 @@
+//namnger bild o kapp och väntar på att de ska ladda innan namn
+window.addEventListener("DOMContentLoaded", () => {
+    const   bild = document.getElementById("img2"); 
+    const   SBtn = document.getElementById("show");
+
+    SBtn.addEventListener("click", () => {
+        if(bild.style.display === "none" || bild.style.display === "") {   // Om bilden är dold → visa den
+        bild.style.display = "block";
+        SBtn.textContent = "X"; // ändra knapptext
+  } 
+        else {     // Om bilden visas → göm den
+        bild.style.display = "none";
+        SBtn.textContent = "övningsprojekt";
+    }
+    
+  });
+
+
+
+});
