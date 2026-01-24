0280d658ec0320a4c262e163e5733bc72082880d
PhilipN55 <228013432+PhilipN55@users.noreply.github.com>
2026-01-24 10:42:58 +0100

    Nystartsida

--- script.js
+++ script.js
@@ -1,20 +1,45 @@
 //namnger bild o kapp och väntar på att de ska ladda innan namn
 window.addEventListener("DOMContentLoaded", () => {
-    const   bild = document.getElementById("img2"); 
-    const   SBtn = document.getElementById("show");
+    const   T = document.getElementById("Tillbaka"); 
+    const   PK = document.getElementById("Projekt");
+    const   OK = document.getElementById("Övningar");
+    const   P = document.querySelectorAll(".a2");
+    const   O = document.querySelectorAll(".a3");
 
-    SBtn.addEventListener("click", () => {
-        if(bild.style.display === "none" || bild.style.display === "") {   // Om bilden är dold → visa den
-        bild.style.display = "flex";
-        SBtn.textContent = "X"; // ändra knapptext
-  } 
-        else {     // Om bilden visas → göm den
-        bild.style.display = "none";
-        SBtn.textContent = "övningsprojekt";
-    }
+    PK.addEventListener("click", () => {
+        if(T.style.visibility === "hidden" || T.style.display === "") {   // Om bilden är dold visa den
+        PK.style.display = "none";
+        OK.style.display = "none";
+        T.style.visibility = "visible";
+        P.forEach(el => {
+          el.style.display = "flex";
+        });
+        } 
+      });
+    OK.addEventListener("click", () => {
+        if(T.style.visibility === "hidden" || T.style.display === "") {
+        PK.style.display = "none";
+        OK.style.display = "none";
+        T.style.visibility = "visible";
+
+        O.forEach(el => {
+          el.style.display = "flex";
+        });
+      }
+    });
+    T.addEventListener("click", () => {
+        if(T.style.visibility === "visible"){
+        PK.style.display = "flex";
+        OK.style.display = "flex";
+        T.style.visibility = "hidden";
+
+        P.forEach(el => {
+          el.style.display = "none";
+        })
+        O.forEach(el => {
+          el.style.display = "none";
+        })
+        }
     
-  });
-
-
-
+    });
 });
